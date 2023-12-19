681df056b9dd99461883b90e028d632a9f9ac91c
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-12-20 02:51:36 +0700

    Update ufs storage life scritps

--- app/src/main/assets/kr-script/other/ufs/ufs_summary.sh
+++ app/src/main/assets/kr-script/other/ufs/ufs_summary.sh
@@ -1,69 +1,86 @@
 if [[ -f /sys/devices/platform/soc/1d84000.ufshc/health_descriptor/life_time_estimation_a ]]; then
-  #qcom
   bDeviceLifeTimeEstA=$(cat /sys/devices/platform/soc/1d84000.ufshc/health_descriptor/life_time_estimation_a)
-elif [[ -f /sys/devices/platform/soc/1d84000.ufshc/health/lifetimeA ]]; then
-  #pixel
-  bDeviceLifeTimeEstA=$(cat /sys/devices/platform/soc/1d84000.ufshc/health/lifetimeA)
-elif [[ -f  /sys/devices/platform/bootdevice/health_descriptor/life_time_estimation_a ]]; then
-  #mtk
-  bDeviceLifeTimeEstA=$(cat  /sys/devices/platform/bootdevice/health_descriptor/life_time_estimation_a)
-elif [[ $(echo /sys/kernel/debug/*.ufshc/dump_health_desc) != "/sys/kernel/debug/*.ufshc/dump_health_desc" ]]; then
+elif [[ -f /sys/devices/virtual/mi_memory/mi_memory_device/ufshcd0/dump_health_desc ]];then
+  bDeviceLifeTimeEstA=$(cat /sys/devices/virtual/mi_memory/mi_memory_device/ufshcd0/dump_health_desc | grep bDeviceLifeTimeEstA | cut -f2 -d '=' | cut -f2 -d ' ')
+else
   bDeviceLifeTimeEstA=$(cat /sys/kernel/debug/*.ufshc/dump_health_desc 2>/dev/null | grep bDeviceLifeTimeEstA | cut -f2 -d '=' | cut -f2 -d ' ')
 fi
 
-# 0x00 No information about the service life of the device was found.
-# 0x01 The estimated life of the device is 0% to 10%.
-# 0x02 The estimated life of the device is 10% to 20%.
-# 0x03 The estimated life of the device is 20% to 30%.
-# 0x04 The estimated life of the device is 30% to 40%.
-# 0x05 The estimated life of the device is 40% to 50%.
-# 0x06 The estimated life of the device is 50% to 60%.
-# 0x07 The estimated life of the device is 60% to 70%.
-# 0x08 The estimated life of the device is 70% to 80%.
-# 0x09 The estimated life of the device is 80% to 90%.
-# 0x0A The estimated life of the device is 90% to 100%.
-# 0x0B The device has exceeded its estimated useful life.
+dump_files=$(find /sys -name "dump_*_desc" | grep ufshc)
+if [[ "$bDeviceLifeTimeEstA" == "" ]];then
+  for line in $dump_files
+  do
+    str=$(grep 'bDeviceLifeTimeEstA' $line | cut -f2 -d '=' | cut -f2 -d ' ')
+    if [[ "$str" != "" ]]; then
+      bDeviceLifeTimeEstA="$str"
+    fi
+  done
+fi
 
-[[ -z "$bDeviceLifeTimeEstA" ]] && { echo "This device has not use ufs nor health info" ; exit 0; }
+if [[ "$bDeviceLifeTimeEstA" == "" ]];then
+  files=$(find /sys -name "life_time_estimation_a" | grep ufshc)
+  for line in $files
+  do
+    str=$(cat $line)
+    if [[ "$str" != "" ]]; then
+      bDeviceLifeTimeEstA="$str"
+    fi
+  done
+fi
+
+# 0x00 No information found about the device's usage lifespan.
+# 0x01 Device estimated lifespan: 0% to 10%.
+# 0x02 Device estimated lifespan: 10% to 20%.
+# 0x03 Device estimated lifespan: 20% to 30%.
+# 0x04 Device estimated lifespan: 30% to 40%.
+# 0x05 Device estimated lifespan: 40% to 50%.
+# 0x06 Device estimated lifespan: 50% to 60%.
+# 0x07 Device estimated lifespan: 60% to 70%.
+# 0x08 Device estimated lifespan: 70% to 80%.
+# 0x09 Device estimated lifespan: 80% to 90%.
+# 0x0A Device estimated lifespan: 90% to 100%.
+# 0x0B Device has exceeded its estimated lifespan.
+
+[[ -z "$bDeviceLifeTimeEstA" ]] && { echo "This device has not used UFS nor provided health information." ; exit 0; }
 
 case $bDeviceLifeTimeEstA in
-"0x00"|"0x00")
-  echo 'Life span unknown'
-;;
+"0x00"|"0x0")
+  echo 'Used lifespan: Unknown'
+  ;;
 "0x01"|"0x1")
-  echo 'Life span 0% ~ 10%'
-;;
+  echo 'Used lifespan: 0% ~ 10%'
+  ;;
 "0x02"|"0x2")
-  echo 'Life span 10% ~ 20%'
-;;
+  echo 'Used lifespan: 10% ~ 20%'
+  ;;
 "0x03"|"0x3")
-  echo 'Life span 20% ~ 30%'
-;;
+  echo 'Used lifespan: 20% ~ 30%'
+  ;;
 "0x04"|"0x4")
-  echo 'Life span 30% ~ 40%'
-;;
+  echo 'Used lifespan: 30% ~ 40%'
+  ;;
 "0x05"|"0x5")
-  echo 'Life span 40% ~ 50%'
-;;
+  echo 'Used lifespan: 40% ~ 50%'
+  ;;
 "0x06"|"0x6")
-  echo 'Life span 50% ~ 60%'
-;;
+  echo 'Used lifespan: 50% ~ 60%'
+  ;;
 "0x07"|"0x7")
-  echo 'Life span 60% ~ 70%'
-;;
+  echo 'Used lifespan: 60% ~ 70%'
+  ;;
 "0x08"|"0x8")
-  echo 'Life span 70% ~ 80%'
-;;
+  echo 'Used lifespan: 70% ~ 80%'
+  ;;
 "0x09"|"0x9")
-  echo 'Life span 80% ~ 90%'
-;;
+  echo 'Used lifespan: 80% ~ 90%'
+  ;;
 "0x0A"|"0xA")
-  echo 'Life span 90% ~ 100%'
-;;
+  echo 'Used lifespan: 90% ~ 100%'
+  ;;
 "0x0B"|"0xB")
-  echo 'Estimated life span has been exceeded'
-;;
+  echo 'Exceeded estimated lifespan'
+  ;;
 *)
-  echo 'Life span unknown'
-;;
+  echo 'Used lifespan: Unknown'
+  ;;
 esac
